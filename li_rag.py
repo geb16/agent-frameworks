6c87408b5e10deff5991b8860049e8a0266b5e29
Danyob16 <deg7@student.london.ac.uk>
2026-02-02 22:58:31 +0000

    Remove unused import statement in li_rag.py for cleaner code

--- li_rag.py
+++ li_rag.py
@@ -18,7 +18,7 @@ from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.llms.openai import OpenAI
 from dotenv import load_dotenv
-import os
+
 
 load_dotenv()
 
